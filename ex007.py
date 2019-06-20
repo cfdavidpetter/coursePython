162b9a9e8ad3f4ade6de8949a6a8be97ac8576fa
David Petter <cfdavidpetter@gmail.com>
2019-06-20 18:10:00 -0300

    media

---
+++ ex007.py
@@ -0,0 +1,4 @@
+numberOne = float(input('Type a number: '))
+numberTwo = float(input('Type another number: '))
+
+print('A media between {:.2f} and {:.2f} is {:.2f}.'.format(numberOne, numberTwo, (numberOne + numberTwo) / 2))
