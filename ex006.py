705c38b2509d9f1857a30bc28c06363d152bbf10
David Petter <cfdavidpetter@gmail.com>
2019-06-20 17:33:20 -0300

    square root

---
+++ ex006.py
@@ -0,0 +1,3 @@
+number = int(input('Type a number: '))
+
+print('The double of {} is worth {}.\nThe triple of {} is worth {}.\nThe square root of {} is equal to {:.2f}.'.format(number, number * 2, number, number * 3, number, number ** (1/2)))
