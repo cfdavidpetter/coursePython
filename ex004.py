b61d61f825cc3f75f8e4752f9c9bcec53d764347
David Petter <cfdavidpetter@gmail.com>
2019-06-20 16:28:35 -0300

    class of type

---
+++ ex004.py
@@ -0,0 +1,10 @@
+someth = str(input('type something: '))
+
+print('The primitive type of this value is? ', type(someth))
+print('Does it have space? ', someth.isspace())
+print('Is it a number? ', someth.isnumeric())
+print('Is it an alphabetic? ', someth.isalpha())
+print('Is it alphabet and number? ', someth.isalnum())
+print('Is this a capital letter? ', someth.isupper())
+print('Is this a lowercase letter? ', someth.islower())
+print('Is this a name? ', someth.istitle())
