6d4d55a379670d9ee34ce12595e5be689b8828d5
David Petter Alves <cfdavidpetter@gmail.com>
2019-06-25 11:47:43 -0300

    Placing several coins

--- ex010.py
+++ ex010.py
@@ -1,9 +1,21 @@
 import urllib.request
 import json
 
+def quotation(coin):
+    contents = urllib.request.urlopen("http://economia.awesomeapi.com.br/json/" + coin + "-BRL/1").read()
+    return float(json.loads(contents)[0]['high'])
+
 real = float(input('How much do you have in your wallet? R$'))
 
-contents = urllib.request.urlopen("http://economia.awesomeapi.com.br/json/USD-BRL/1").read()
-quotation = float(json.loads(contents)[0]['high'])
+coins = [
+    ('USD', 'dollar'),
+    ('EUR', 'euro'),
+    ('GBP', 'pound sterling'),
+    ('ARS', 'argentine peso'),
+    ('JPY', 'japanese yen'),
+    ('CHF', 'swiss franc'),
+    ('CNY', 'chinese yuan'),
+]
 
-print('In the current quotation of the dollar you have US$:{:.2f}'.format(real / quotation))
+for coin in coins:
+    print('In the current quotation of the {} you have $:{:.2f}'.format(coin[1], real / quotation(coin[0])))
